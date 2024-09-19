9ae9fa4da4725a7ef45233d3292cd63b85b22af4
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-09-19 14:14:58 -0400

    swiper faune et flore

--- js/script.js
+++ js/script.js
@@ -16,3 +16,21 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+
+var swiper = new Swiper(".mySwiper3", {
+  loop: true,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper4", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
