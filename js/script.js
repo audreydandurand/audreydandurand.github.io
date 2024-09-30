5959e6d9d7ea591d5324bd5ee31e0bc10e59f9bc
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-09-30 14:02:41 -0400

    swiper 2-3-4

--- js/script.js
+++ js/script.js
@@ -34,3 +34,39 @@ var swiper2 = new Swiper(".mySwiper4", {
     swiper: swiper,
   },
 });
+
+var swiper = new Swiper(".mySwiper5", {
+  loop: true,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper6", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper = new Swiper(".mySwiper7", {
+  loop: true,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper8", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
